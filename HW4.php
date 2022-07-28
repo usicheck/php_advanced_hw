96e2e7a9e0afc2dd59e14b9512548b0192399a6a
usicheck <104168720+usicheck@users.noreply.github.com>
2022-07-28 16:07:55 +0300

    Update HW4.php

--- HW4.php
+++ HW4.php
@@ -2,7 +2,7 @@
 
 class Color
 {
-    private int $red, $green, $blue;
+    private float $red, $green, $blue;
 
     public function __construct($red, $green, $blue)
     {
@@ -11,72 +11,117 @@ class Color
         $this->blue = $blue;
 
     }
-    public function mix(Color $color2): void
-    {
-        $r=(($this->setRed())+($color2->setRed()))/2;
-        $g=(($this->setGreen())+($color2->setGreen()))/2;
-        $b=(($this->setBlue())+($color2->setBlue()))/2;
 
-        echo $r.' '. $g.' '.$b;
-//        return $arr=[$r,$g,$b];
+    public function mix(Color $color2): object
+    {
+        $r = (($this->setRed()) + ($color2->setRed())) / 2;
+        $g = (($this->setGreen()) + ($color2->setGreen())) / 2;
+        $b = (($this->setBlue()) + ($color2->setBlue())) / 2;
+
+        return new Color ($r, $g, $b);
+
     }
 
-    public function getRed():void
+    public function getRed(): void
     {
         echo $this->setRed();
     }
 
 
-    public function getGreen():void
+    public function getGreen(): void
     {
         echo $this->setGreen();
     }
 
-    public function getBlue():void
+    public function getBlue(): void
     {
         echo $this->setBlue();
     }
-    private function setRed(): int
+
+    private function setRed(): float
     {
         if (($this->red) < 0 || ($this->red) > 255) {
-            throw new Exception('Number must be in diapazon 0...255');
+            try {
+                throw new Exception("Номер кольору має бути у діапазоні 0...255.");
+            } catch (Exception $e) {
+                echo $e->getMessage() . '<br>';
+            }
+        } else {
+            $red = $this->red;
         }
-        return $this->red;
-    }
-    private function setGreen(): int
-    {
-        if (($this->green)< 0 || ($this->green) > 255) {
-            throw new Exception('Number must be in diapazon 0...255');
-        }
-        return $this->green;
+        return $red;
     }
 
-    private function setBlue():int
+    private function setGreen(): float
+    {
+        if (($this->green) < 0 || ($this->green) > 255) {
+            try {
+                throw new Exception("Номер кольору має бути у діапазоні 0...255.");
+            } catch (Exception $e) {
+                echo $e->getMessage() . '<br>';
+            }
+        } else {
+            $green = $this->green;
+        }
+        return $green;
+    }
+
+    private function setBlue(): float
     {
         if (($this->blue) < 0 || ($this->blue) > 255) {
-            throw new Exception('Number must be in diapazon 0...255');
+            try {
+                throw new Exception("Номер кольору має бути у діапазоні 0...255.");
+            } catch (Exception $e) {
+                echo $e->getMessage() . '<br>';
+            }
+        } else {
+            $blue = $this->blue;
         }
-        return $this->blue;
+        return $blue;
     }
 
-//    public function displayInfo(): void
-//    {
-//        echo "R: $this->red; G: $this->green; B: $this->blue.<br>";
-//
-//    }
+    public function equals($color1, $color2): bool
+    {
+        if ($color1 == $color2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function random($colorRandom): object
+    {
+        $colorRandom->red = random_int(0, 255);
+        $colorRandom->green = random_int(0, 255);
+        $colorRandom->blue = random_int(0, 255);
+
+        return $colorRandom;
+    }
 
 }
 
+$color1 = new Color(200, 100, 100);
+echo 'Перший колір в RGB: ' . '<br>';
+echo $color1->getRed() . '<br>';
+echo $color1->getGreen() . '<br>';
+echo $color1->getBlue() . '<br>';
 
-$color1 = new Color(200, 200, 100);
-$color2 = new Color(167, 112, 113);
-$color3=$color1->mix($color2);
-//$color3->r;
-//$color3->getRed();
+$color2 = new Color(104, 97, 45);
+echo 'Другий колір в RGB: ' . '<br>';
+echo $color2->getRed() . '<br>';
+echo $color2->getGreen() . '<br>';
+echo $color2->getBlue() . '<br>';
 
+$colorMixed = $color1->mix($color2);
+echo 'Мікс кольорів (середнє значення) в RGB: ' . '<br>';
+echo $colorMixed->getRed() . '<br>';
+echo $colorMixed->getGreen() . '<br>';
+echo $colorMixed->getBlue() . '<br>';
 
-//
-//$mixedColor = $color->mix(new Color(100, 100, 100));
-//$mixedColor->getRed(); // 150
-//$mixedColor->getGreen(); // 150
-//$mixedColor->getBlue(); // 150
+$colorRandom = new Color(0, 0, 0);
+echo 'Рандомне значення кольорів в RGB: ' . '<br>';
+
+print_r($colorRandom->random($colorRandom));
+
+echo '<br>'.'Чи однакові значення мають об\'єкти: ' . '<br>';
+var_dump ($colorMixed->equals($color1, $color2));
